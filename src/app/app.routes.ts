f5515f1dced25da1672238b5247633756bf4531f
ViestursK <95314459+ViestursK@users.noreply.github.com>
2025-07-09 09:07:00 +0300

    create project structure folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,5 @@
 import { Routes } from '@angular/router';
-import { Landing } from './pages/landing/landing';
-import { Home } from './pages/home/home';
-import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-  { path: '', component: Landing },
-  { path: 'home', component: Home },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Main } from './main/main';
+import { SideNav } from './side-nav/side-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header, Main, SideNav],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected title = 'ticken-management-frontend';
