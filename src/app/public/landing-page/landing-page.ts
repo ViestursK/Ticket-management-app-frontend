bd71c09f67c84878499f06ff4597001b454767f0
ViestursK <95314459+ViestursK@users.noreply.github.com>
2025-07-09 21:27:58 +0300

    created landing page draft

--- src/app/public/landing-page/landing-page.ts
+++ src/app/public/landing-page/landing-page.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { LandingCta } from './landing-cta/landing-cta';
+import { LandingFeatures } from './landing-features/landing-features';
+import { LandingFooter } from './landing-footer/landing-footer';
+import { LandingHero } from './landing-hero/landing-hero';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [LandingCta, LandingFeatures, LandingFooter, LandingHero],
   templateUrl: './landing-page.html',
-  styleUrl: './landing-page.css'
+  styleUrl: './landing-page.css',
 })
 export class LandingPage {
-
+  
 }
